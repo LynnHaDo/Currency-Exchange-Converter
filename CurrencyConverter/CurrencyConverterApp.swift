958d61238e059441a38512e6eb09ae63f63d89bf
Lynn Do <do24l@mtholyoke.edu>
2024-12-21 11:47:38 -0800

    Initial Commit

---
+++ CurrencyConverter/CurrencyConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurrencyConverterApp.swift
+//  CurrencyConverter
+//
+//  Created by Do Linh on 12/21/24.
+//
+
+import SwiftUI
+
+@main
+struct CurrencyConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
